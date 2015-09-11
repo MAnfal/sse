0bc65b1b65980426ba885cfef8a903aac71baf73
Muhammad Anfal <muhammadanfal0@gmail.com>
2015-09-11 04:18:22 -0700

    Update (Under development)

---
+++ src/SSEServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MAnfal\sse;
+
+use Illuminate\Support\ServiceProvider;
+
+class SSEServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
